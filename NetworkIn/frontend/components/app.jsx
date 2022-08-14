935bca2b93153b431ace00be35ee95fa044ff838
Anthony Chao <anthonyschaowder@gmail.com>
2022-08-13 22:47:31 -0400

    added homepage placeholder

--- NetworkIn/frontend/components/app.jsx
+++ NetworkIn/frontend/components/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+import LoginFormContainer from './session/login_form_container'
+import SignupFormContainer from './session/signup_form_container'
+import HomepageContainer from './homepage/homepage_container'
 
 const App = () => (
     <div>
         <h1>NetworkIn</h1>
+        <HomepageContainer />
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
     </div>
 );
 
---
+++ NetworkIn/frontend/components/homepage/homepage_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import Homepage from './homepage';
+
+const mapStateToProps = ({ session, entities: { users } }) => ({
+    currentUser: users[session.id]
+});
+
+const mapDispatchToProps = dispatch => ({
+    logout: () => dispatch(logout())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
